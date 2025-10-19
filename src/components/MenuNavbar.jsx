8e1e14a3becf0aae9efe106de4d277f8ce87e319
Angel YG <angel.yarang4@gmail.com>
2025-10-19 12:27:23 -0500

    Change menu links from href to Link

--- src/components/MenuNavbar.jsx
+++ src/components/MenuNavbar.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 
 function MenuNavbar() {
   return (
-    <Navbar expand="md" >
-        <Container>
-          <Navbar.Brand href="/">R&M</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/character">Characters</Nav.Link>
-              <Nav.Link href="/location">Locations</Nav.Link>
-              <Nav.Link href="/episode">Episodes</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-  )
+    <Navbar expand="md">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          R&M
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/character">
+              Characters
+            </Nav.Link>
+            <Nav.Link as={Link} to="/location">
+              Locations
+            </Nav.Link>
+            <Nav.Link as={Link} to="/episode">
+              Episodes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default MenuNavbar
